f9b5b5b90031ae106b264e7b7532c112bd185465
Mickey Alperin <mjalperin@gmail.com>
2016-11-05 12:01:24 -0400

    more stuff

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-//const request = require('request')
+const request = require('request')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 
 // for facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'this_is_the_correct_token') {
+	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
