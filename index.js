800755aeaf2c65d4cd8f11ee3696634a48b5c667
Mickey Alperin <mjalperin@gmail.com>
2016-11-11 13:56:08 -0500

    added insert items method

--- index.js
+++ index.js
@@ -23,6 +23,18 @@ app.get('/', function (req, res) {
 	res.send('hello world i am a secret bot')
 })
 
+
+function Inserts(template, data) {
+    if (!(this instanceof Inserts)) {
+        return new Inserts(template, data);
+    }
+    this._rawDBType = true;
+    this.formatDBType = function () {
+        return data.map(d=>'(' + pgp.as.format(template, d) + ')').join(',');
+    };
+}
+
+
 // for facebook verification
 app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
@@ -89,58 +101,29 @@ function addNewUser(senderID, text) {
 	})
 }
 
-function sendTextMessage(sender, text) {
-	let messageData = { text:text }
+function insertNewItems(senderID, itemArray) {
 
-	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
-		qs: {access_token:token},
-		method: 'POST',
-		json: {
-			recipient: {id:sender},
-			message: messageData,
+	pg.connect(conString, function (err, client, done) {
+		if (err) {
+			return console.error('error fetching client from pool', err)
 		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log('Error sending messages: ', error)
-		} else if (response.body.error) {
-			console.log('Error: ', response.body.error)
+
+		for(var i = 0; i < itemArray.length; i++) {
+			client.query('INSERT INTO PantryItems (user_id, item_name) VALUES ($1, $2);', [senderID, itemArray[i]], function (err, result) {
+				done()
+				if (err) {
+					return console.error('error happened during query', err)
+				}
+			})
 		}
+
 	})
 }
 
-function sendGenericMessage(sender) {
-	let messageData = {
-		"attachment": {
-			"type": "template",
-			"payload": {
-				"template_type": "generic",
-				"elements": [{
-					"title": "First card",
-					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://www.messenger.com",
-						"title": "web url"
-					}, {
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for first element in a generic bubble",
-					}],
-				}, {
-					"title": "Second card",
-					"subtitle": "Element #2 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-					"buttons": [{
-						"type": "postback",
-						"title": "Postback",
-						"payload": "Payload for second element in a generic bubble",
-					}],
-				}]
-			}
-		}
-	}
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+
 	request({
 		url: 'https://graph.facebook.com/v2.6/me/messages',
 		qs: {access_token:token},
