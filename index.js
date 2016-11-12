5d04698e1240f0d24e166c40d7ac1ec71a4fd6c6
Mickey Alperin <mjalperin@gmail.com>
2016-11-12 16:47:52 -0500

    rrrrrg

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function sendMessageToWatson(text, senderID, context) {
 		var payload = {
 	    workspace_id: workspace,
 	    context: context,
-	    input: text
+	    input: text.substring(0,200)
 	  };
 
 	  // Send the input to the conversation service
