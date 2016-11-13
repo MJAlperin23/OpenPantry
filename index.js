98e0b56dbd31db4431dc91805179988bf8ffc603
aphilipps <aphilipps9@gmail.com>
2016-11-12 23:06:16 -0500

    fixed back to before

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ function determineNext(senderID, data) {
 
       search(senderID, tot.toString(), function(data) {
           getRecipe(senderID, data.recipes[0].recipe_id, function(recipe) {
-              String recipe_ingred = recipe.recipe.ingredients.toString();
+              let recipe_ingred = recipe.recipe.ingredients.toString();
               console.log(recipe_ingred);
               sendMessageToWatson(recipe_ingred, senderID);
           })
